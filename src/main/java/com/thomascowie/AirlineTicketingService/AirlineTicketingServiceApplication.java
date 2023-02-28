31ed1568254b1539d86c69f1a860252edce6e3f9
Thomas <thomas.cowie12@gmail.com>
2023-02-27 21:15:04 -0500

    Initial commit

---
+++ src/main/java/com/thomascowie/AirlineTicketingService/AirlineTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thomascowie.AirlineTicketingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomascowie/AirlineTicketingService/AirlineTicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomascowie.AirlineTicketingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
